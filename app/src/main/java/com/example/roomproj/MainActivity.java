e6690acc11c12de365c374db673ec8099e7a96eb
Álvaro Zamora <alvarozajim@gmail.com>
2019-11-21 13:24:35 +0100

    recyclerview and listadapter

--- app/src/main/java/com/example/roomproj/MainActivity.java
+++ app/src/main/java/com/example/roomproj/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.roomproj;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import com.example.roomproj.astronauta.AstronautaListAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        final AstronautaListAdapter adapter = new AstronautaListAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ app/src/main/java/com/example/roomproj/astronauta/AstronautaListAdapter.java
@@ -0,0 +1,63 @@
+package com.example.roomproj.astronauta;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.roomproj.R;
+import com.example.roomproj.db.Astronauta;
+
+import java.util.List;
+
+public class AstronautaListAdapter extends RecyclerView.Adapter<AstronautaListAdapter.AstronautaViewHolder> {
+
+    class AstronautaViewHolder extends RecyclerView.ViewHolder {
+        private final TextView astronautaItemView;
+
+        private AstronautaViewHolder(View itemView) {
+            super(itemView);
+            astronautaItemView = itemView.findViewById(R.id.textView);
+        }
+    }
+
+    private final LayoutInflater mInflater;
+    private List<Astronauta> mAstronautas; // Cached copy of words
+
+    public AstronautaListAdapter(Context context) { mInflater = LayoutInflater.from(context); }
+
+    @Override
+    public AstronautaViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.activity_main, parent, false);
+        return new AstronautaViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(AstronautaViewHolder holder, int position) {
+        if (mAstronautas != null) {
+            Astronauta current = mAstronautas.get(position);
+            // name here was set as a get() method
+            holder.astronautaItemView.setText(current.name);
+        } else {
+            // Covers the case of data not being ready yet.
+            holder.astronautaItemView.setText("No Word");
+        }
+    }
+
+    void setWords(List<Astronauta> astronautas){
+        mAstronautas = astronautas;
+        notifyDataSetChanged();
+    }
+
+    // getItemCount() is called many times, and when it is first called,
+    // mWords has not been updated (means initially, it's null, and we can't return null).
+    @Override
+    public int getItemCount() {
+        if (mAstronautas != null)
+            return mAstronautas.size();
+        else return 0;
+    }
+}
